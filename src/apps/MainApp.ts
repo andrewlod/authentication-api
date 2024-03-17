6d0f0801b4b24f40dc9fde3e3f7486b6451cb41a
Andre Wlodkovski <andremacwlod@gmail.com>
2024-03-17 09:16:04 -0300

    Implement authorization cookies

--- src/apps/MainApp.ts
+++ src/apps/MainApp.ts
@@ -3,10 +3,12 @@ import type { RequestHandler } from 'express'
 import cors from 'cors'
 import { AccountRouter, UserRouter } from '../routers'
 import { AccessController } from '../controllers'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/v1/account', AccountRouter)
 
--- src/controllers/AccessController.ts
+++ src/controllers/AccessController.ts
@@ -1,13 +1,25 @@
 import type { NextFunction, Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { JWTManager } from '../auth'
+import { SecretManager } from '../secrets'
+
+const JWT_COOKIE_KEY = SecretManager.getSecret('JWT_COOKIE_KEY')
 
 interface AuthorizationHeader {
   authorization?: string
 }
 
+type AuthorizationCookie = {
+  [JWT_COOKIE_KEY in string]: string
+}
+
 export async function isAuthenticated (req: Request, res: Response, next: NextFunction): Promise<void> {
-  const { authorization } = req.headers as AuthorizationHeader
+  let { authorization } = req.headers as AuthorizationHeader
+
+  if (authorization === undefined) {
+    const cookies = req.cookies as AuthorizationCookie
+    authorization = cookies[JWT_COOKIE_KEY]
+  }
 
   if (authorization === undefined) {
     res.status(StatusCodes.UNAUTHORIZED).json({
