2a6eef08a35e0bc3bc3dc72c43339d836959078e
Andre Wlodkovski <andremacwlod@gmail.com>
2024-03-22 19:06:06 -0300

    Add Route Not Found test

--- tests/app/ApiInfo.test.ts
+++ tests/app/ApiInfo.test.ts
@@ -5,6 +5,7 @@ import fs from 'fs'
 import path from 'path'
 
 import { MainApp } from "../../src/apps"
+import { ErrorConstants } from '../../src/errors'
 
 describe('Health Check [V1]', () => {
   test('Ping', async () => {
@@ -23,10 +24,22 @@ describe('Health Check [V1]', () => {
     const response = await request(MainApp)
       .post('/api/v1/api-info/version')
       .send()
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
     
       expect(response.statusCode).toBe(StatusCodes.OK)
       expect(response.text).toBe(`Authentication API v${apiVersion}`)
+  })
+
+  test('Route Not Found', async () => {
+    const packageJson = fs.readFileSync(path.join(__dirname, '../../package.json'))
+    const apiVersion = JSON.parse(packageJson.toString('utf-8')).version
+
+    const response = await request(MainApp)
+      .post('/api/v1/api-info/this-route-does-not-exist')
+      .send()
+    
+      expect(response.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(response.body).toHaveProperty('error')
+      expect(response.body.error).toHaveProperty('code')
+      expect(response.body.error.code).toBe(ErrorConstants.ROUTE_NOT_FOUND)
   })
 })
